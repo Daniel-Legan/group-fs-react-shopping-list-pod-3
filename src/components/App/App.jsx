f348c45cf20c6f404ab479cfbb39e3aa10c9f728
Tia Theisen <tiatheisen@gmail.com>
2022-10-20 11:48:56 -0500

    linked app.jsx to grocerylist component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
+import GroceryList from '../GroceryList/GroceryList.jsx';
 
 
 function App() {
